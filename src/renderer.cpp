bc50705b1e736f92ef8a2196a63b3e40a448e506
matt <bassetmatt@gmail.com>
2023-04-07 19:26:11 -0400

    Update build system and remove prints

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -30,7 +30,6 @@ void DisplayManager::getColors() {
 			.g = (Uint8) g,
 			.b = (Uint8) b,
 			.a = 255 };
-		printf("r, g, b = (%d, %d, %d)\n", r, g, b);
 	}
 	fileStream.close();
 }
@@ -60,10 +59,6 @@ void DisplayManager::drawLoop() {
 						case SDLK_ESCAPE:
 							running = false;
 							break;
-						case SDLK_l:
-							this->renderLines = !this->renderLines;
-							this->blocksRenderered = false;
-							break;
 					}
 					break;
 			}
@@ -71,34 +66,16 @@ void DisplayManager::drawLoop() {
 		SDL_SetRenderDrawColor(g_renderer, 255, 255, 255, 255);
 		SDL_RenderClear(g_renderer);
 		this->drawBlocks();
-		if (!this->blocksRenderered) {
-			this->blocksRenderered = true;
-		}
-		// if (this->renderLines)
-		// 	this->drawLines();
 		SDL_RenderPresent(g_renderer);
 	}
 }
 
 void DisplayManager::drawBlocks() {
 	int Nblocks = this->p_blockList->size();
-	if (!this->blocksRenderered) {
-		printf("Table: ");
-		for (Point p : coordTable)
-			printf("(%ld, %ld)", p.x, p.y);
-		printf("\n");
-		printf("N blocks: %d\n", Nblocks);
-		printf("Max Coord = %d\n", this->maxCoord);
-		printf("Block size: %d\n", this->blockSize);
-	}
 	for (int i = 0; i < Nblocks; ++i) {
 		int j0, s;
 		(*p_blockList)[i].getFirstIdAndSize(&j0, &s);
 		SDL_Color c = this->colList[i % 100];
-		if (!this->blocksRenderered) {
-			printf(" Block %d: first id = %d, size = %d\n", i, j0, s);
-			printf("Current Color: :%u-%u-%u\n", c.r, c.g, c.b);
-		}
 		SDL_SetRenderDrawColor(g_renderer, c.r, c.g, c.b, c.a);
 		for (int j = j0; j < j0 + s; ++j) {
 			Point p = coordTable[j];
@@ -109,8 +86,4 @@ void DisplayManager::drawBlocks() {
 			SDL_RenderFillRect(g_renderer, &r);
 		}
 	}
-}
-
-void DisplayManager::drawLines() {
-
 }
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -7,24 +7,20 @@
 #include "structures.hpp"
 #include "utils.hpp"
 
-// SDL_Window* g_window = NULL;
-// SDL_Renderer* g_renderer = NULL;
 using ColorList = std::vector<SDL_Color>;
+
 class DisplayManager {
 private:
 	int maxCoord;
 	int blockSize;
-	bool renderLines = true;
 	BlockList* p_blockList;
-	bool blocksRenderered  = false;
-public:
 	ColorList colList;
+public:
 	DisplayManager(int size, BlockList* p_blockList);
 	void initRenderer();
 	void drawLoop();
 private:
 	void getColors();
 	void drawBlocks();
-	void drawLines();
 };
 #endif /* TP3_RENDERER_HEADER */
